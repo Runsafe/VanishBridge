8495f6cde24d3edb086b35944b3e32bf5a0daf49
Morten Nilsen <morten@runsafe.no>
2012-04-14 22:55:24 +0200

    Add files to project

---
+++ src/no/runsafe/bridge/VanishNoPacket.java
@@ -0,0 +1,12 @@
+package no.runsafe.bridge;
+
+import no.runsafe.framework.RunsafePlugin;
+
+public class VanishNoPacket extends RunsafePlugin
+{
+	@Override
+	protected void PluginSetup()
+	{
+
+	}
+}
