99688fd5b9f5df21ac48a4df7dff711be15b22b1
Morten Nilsen <morten@runsafe.no>
2013-07-19 03:06:46 +0200

    Need a list here..

--- src/no/runsafe/vanishbridge/EventFactory.java
+++ src/no/runsafe/vanishbridge/EventFactory.java
@@ -1,12 +1,13 @@
 package no.runsafe.vanishbridge;
 
 import no.runsafe.framework.internal.wrapper.ObjectWrapper;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 public class EventFactory
 {
 	public void Fire(Player player, boolean vanished)
 	{
-		new VanishEvent(ObjectWrapper.convert(player), vanished).Fire();
+		new VanishEvent(ObjectWrapper.convert((OfflinePlayer) player), vanished).Fire();
 	}
 }
